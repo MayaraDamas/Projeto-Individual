ea9e1901051b5c06c24feb59d43f27e187fc74b4
MayaraDamas <mayara.damas@sptech.school>
2024-06-06 11:41:21 -0300

    Subindo atualizações recentes

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -28,7 +28,7 @@ function CalcularPontuacao(usuario, quiz, pontuacao) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO Pontuacao (fkUsuario, fkQuiz, Pontuacao, Data) VALUES ('${usuario}', '${quiz}', '${pontuacao}', NOW());
+        INSERT INTO Pontuacao (fkUsuario, fkQuiz, Pontuacao, Data) VALUES (${usuario}, ${quiz}, ${pontuacao}, NOW());
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
